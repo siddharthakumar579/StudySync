71e7480ef6a8d4ee39a660c39a81222e78e37c07
Siddharth Kumar <siddharthakumar579@gmail.com>
2026-03-24 13:54:18 +0530

    Add dark mode switcher
    Implement theme toggle functionality with event listener.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+let container = document.querySelector('.container')
+        let circle = document.querySelector('.circle')
 
+        let lightTheme = true
+
+        circle.addEventListener('click' , function themeChange() {
+            if (lightTheme) {
+                document.body.style.backgroundColor = '#584f4f'
+                // circle.style.backgroundColor = '#ffffff'
+                document.querySelector('.testimonial-container').style.backgroundColor = '#584f4f'
+                document.querySelector('.feature-container').style.backgroundColor = '#584f4f'
+                document.querySelector('.testimonial-card-image').style.backgroundColor = '#584f4f'
+                document.querySelector('#card2').style.backgroundColor = '#584f4f'
+                document.querySelector('#card3').style.backgroundColor = '#584f4f'
+            }else{
+                document.body.style.backgroundColor = '#ffffff'
+                // circle.style.backgroundColor = 'black'
+                document.querySelector('.testimonial-container').style.backgroundColor = '#ffffff'
+                document.querySelector('.feature-container').style.backgroundColor = '#ffffff'
+                document.querySelector('.testimonial-card-image').style.backgroundColor = '#ffffff'
+                document.querySelector('#card2').style.backgroundColor = '#ffffff'
+                document.querySelector('#card3').style.backgroundColor = '#ffffff'
+            }
+
+            lightTheme = !lightTheme
+        })
+
+       
+
+        
